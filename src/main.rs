18cc0d587f774552a0eab14c8cf0311357d0179b
Harish R <dev@rhrish.com>
2023-01-18 13:05:29 +0100

    Implemented command line help, and echo features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Harish <dev@rhrish.com>")
+        .about("Rust Echo")
+        .arg(
+            Arg::with_name("text")
+            .value_name("TEXT")
+            .help("Input text")
+            .required(true)
+            .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+            .short("n")
+            .help("Do not print newline")
+            .takes_value(false)
+        )
+        .get_matches();
+        let text = matches.values_of_lossy("text").unwrap();
+        let newline = if matches.is_present("omit_newline") { "" } else { "\n" };
+        print!("{}{}",text.join(" "),newline);
 }
